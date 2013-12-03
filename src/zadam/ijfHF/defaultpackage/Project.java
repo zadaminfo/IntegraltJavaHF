6a2ded3151183910df338c7e1f7867555a9d16c7
azoldesi <zoldesi.adam@gmail.com>
2013-12-03 13:15:58 +0100

    Database schema created, JPA is working

---
+++ src/zadam/ijfHF/defaultpackage/Project.java
@@ -0,0 +1,57 @@
+package zadam.ijfHF.defaultpackage;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Project")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private int id;
+     
+    @Column
+    private String name;
+     
+    @Column
+    private String description;
+     
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof User){
+            User user = (User) obj;
+            return user.getId() == this.getId();
+        }
+         
+        return false;
+    }
+ 
+    public int getId() {
+        return id;
+    }
+ 
+    public void setId(int id) {
+        this.id = id;
+    }
+ 
+    public String getName() {
+        return name;
+    }
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+ 
+    public String getDescription() {
+        return description;
+    }
+ 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
